22c0e456f3137a714f64d0727a9c329ee12ff016
Alex Frappier Lachapelle <narutoxela@gmail.com>
2020-03-06 16:55:08 -0500

    prolog

--- Assignment #2/prolog-template.pl
+++ Assignment #2/prolog-template.pl
@@ -64,10 +64,73 @@ superstate(parkedMode, parked).
 superstate(manualDrivingMode, drivingMode).
 superstate(manualDrivingMode, breakMode).
 
-%% TODO, panic mode
-superstate(panicMode, null).
+superstate(panicMode, panicInit).
+superstate(panicMode, panic).
+superstate(panicMode, panicParked).
+
+superstate(cruiseMode, cruise).
+superstate(cruiseMode, tailingMode).
+superstate(cruiseMode, changeLane).
+superstate(cruiseMode, accelerate).
+superstate(cruiseMode, decelerate).
+superstate(cruiseMode, laneChange).
+superstate(cruiseMode, turn).
+superstate(cruiseMode, destinationAhead).
+superstate(cruiseMode, destinationArrived).
 
 
+transition(idle, parkedMode, ignition, null, null).
+transition(parkedMode, null, 'turn off', null, null).
+transition(parkedMode, manualDrivingMode, beep, 'destination not set', null).
+transition(parkedMode, manualDrivingMode, null, 'engine is idle', null).
+transition(parkedMode, cruiseMode, null, 'destination is set', null).
+transition(manualDrivingMode, parkedMode, null, null, null).
+transition(cruiseMode, parkedMode, null, null, null).
+transition(cruiseMode, manualDrivingMode, 'switch to manual', null, null).
+transition(cruiseMode, panicMode, null, 'unforseen event', 'car is stopped ; hazard signal is turned ON').
+transition(cruiseMode, parkedMode, 'turn off panic', 'hazard signal is turned OFF').
+
+transition(parked, parked, 'set destination', 'the navigation system destination is set', null).
+
+transitiom(drivingMode, drivingMode, 'accelerate', 'speed < max', 'engine runs faster').
+transitiom(drivingMode, drivingMode, 'reduce speed', 'speed > 0', 'engine runs slower').
+transition(drivingMode, breakMode, 'break', null, 'engine is stopped').
+transition(breakMode, drivingMode, 'accelerate', null, 'engine runs faster').
+
+transition(cruising, cruising, 'Maintain Speed', 'Distance > ThresholdLimit', null).
+transition(cruising, changeLane, 'Change one lane left', 'ObstacleNotMoving or SafeDistanceNotAttainable', null).
+transition(cruising, tailingMode, 'slow down', 'Distance < ThresholdLimit', null).
+transition(tailingMode, 'maintain speed', 'distance > ThresholdLimit', null).
+transition(changeLane, cruising, null, 'LaneChangeFinished', null).
+
+transition(cruising, cruising, 'maintain speed', 'CurrentSpeed > 95% of default speed and CurrentSpeed < 105% of default speed', null).
+transition(cruising, accelerate, 'accelerate', 'CurrentSpeed < MinimalSpeed', null).
+transition(cruising, decelerate, 'decelerate', 'CurrentSpeed > MaximalSpeed', null).
+transition(accelerate, cruising, 'Maintain Speed', 'CurrentSpeed > 95% of default speed and CurrentSpeed < 105% of default speed', null).
+transition(decelerate, cruising, 'Maintain Speed', 'CurrentSpeed > 95% of default speed and CurrentSpeed < 105% of default speed', null).
+
+trantition(panicInit, panic, 'Manual Signal On', 'Stop and Hazards On', null).
+trantition(panicInit, panic, 'Automatic Signal On', 'Stop and Hazards On', null).
+transition(panic, panicParked, 'panic signal off', 'UserTurnedPanicSignalOff', null).
+
+transition(cruising, laneChange, 'signal off', 'IsLeftmost or IsRightMost', null).
+transition(cruising, laneChange, 'change to rightmost lane', 'IsRightSignalOn', null).
+transition(cruising, destinationAhead, 'Turn on Destination Ahead Signal', 'IsDestinationAhead', null).
+transition(cruising, turn, 'right turn', 'UpcomingIntersection and IsRightSignalOn', null).
+transition(cruising, turn, 'left turn', 'UpcomingIntersection and IsLeftSignalOn', null).
+transition(laneChange, cruising, 'signal off', 'IsLeftmost or IsRightMost', null).
+transition(turn, cruising, null, 'IsTurnFinished', null).
+transition(turn, 'Destination Arrived', 'Turn off Signal, Turn right and Stop', 'IsRightMost and DestinationAheadSignalsOn', null).
+transition(destinationAhead, laneChange, 'Change to rightmost lane', 'isNotRightMost', null).
+transition(destinationAhead, turn, null, 'isRightMost', null).
+
+transition(cruising, laneChange, null, 'RightSignalOn', null).
+transition(cruising, laneChange, null, 'LeftSignalOn', null).
+transition(cruising, panic, 'Panic Mode turned on', 'LeftLaneNotOpen and RightLaneNotOpen and ObstacleDetected', null).
+trantition(laneChange, laneChange, null, 'LeftLaneNotOpen', null).
+trantition(laneChange, laneChange, null, 'RightLaneNotOpen', null).
+trantition(laneChange, laneChange, 'Change lane', 'LeftLaneOpen', null).
+trantition(laneChange, laneChange, 'Change lane', 'RightLaneOpen', null).
 
 
 %% =============================================================================
@@ -75,6 +138,8 @@ superstate(panicMode, null).
 %%  Rules
 %%
 %% =============================================================================
+
+
 
 
 %% eof.
